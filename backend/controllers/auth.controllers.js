5c37d55173ff2c889e043264b1c94681f7034715
krishbhutani <123577916+krishbhutani@users.noreply.github.com>
2025-10-28 02:19:16 +0530

    Change SameSite cookie attribute to 'None'
    Updated SameSite attribute for cookies to 'None' for cross-site usage.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,7 +27,7 @@ const token=await genToken(user._id)
 res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"Strict",
+    sameSite:"None",
     secure:true
    })
 
@@ -56,7 +56,7 @@ export const login=async (req,res)=>{
  res.cookie("token",token,{
      httpOnly:true,
      maxAge:7*24*60*60*1000,
-     sameSite:"Strict",
+     sameSite:"None",
      secure:true
     })
  
